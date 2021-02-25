a6b3f05cace12b58f5fb1d07eea0efdf69d132f4
Euge Borghello <craeugeniaborghello@gmail.com>
2021-02-25 18:21:26 -0300

    estilo navbar

--- client/src/components/SearchBar.jsx
+++ client/src/components/SearchBar.jsx
@@ -1,7 +1,8 @@
 import { useState } from "react";
 import { Link } from "react-router-dom";
 import { AppBar, Button, makeStyles, Toolbar, Typography } from '@material-ui/core';
-import s from './Styles/SearchBar.module.css'
+import s from './Styles/SearchBar.module.css';
+import logoML from '../assets/logoML.png'
 
 //import purple from '@material-ui/core/colors/purple';
 //import green from '@material-ui/core/colors/green';
@@ -33,28 +34,35 @@ function SearchBar({ onSearch }) {
 
   return (
     <div className={s.searchBar}>
-      <div>
+      <div className={s.navLogo}>
         <Link to="/">
-          <h3 className={s.text}>Home</h3>
+          
+          <img  
+                id="logo" src={logoML} className={s.img} width="20%" height="20%" alt="" 
+                />
+            
         </Link>
       </div>
       <form
-        className="form"
+        className={s.form}
         onSubmit={(e) => {
           e.preventDefault();
           onSearch(input);
           setInput("");
         }}
       >
-        <input className="button-submit" type="submit" value="Search" />
-        <input
+        
+        <input className={s.input}
           type="search"
           placeholder="Search product..."
           onChange={(e) => setInput(e.target.value)}
           value={input}
         />
+        <input className={s.buttonSubmit} type="submit" value="Search" />
       </form>
-      <ShoppingCartOutlinedIcon className={s.cart}/>
+      <div className={s.contenedorCart}>
+        <ShoppingCartOutlinedIcon className={s.cart}/>
+      </div>
     </div>
   );
 }
